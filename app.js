22459e833ab1bc250f031c65d0075d675aae30e6
E-Martinez77 <emartizzle2185@me.com>
2021-11-29 21:41:21 -0700

    new errors

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const userAnswer = document.getElementById("answer");
 const total = document.getElementById("total");
 const type = document.getElementById("type");
 
+const finalOutput = document.getElementById("final-output");
+
 const form = document.getElementById("form");
 
 form.addEventListener("submit", (e) => {
@@ -29,58 +31,59 @@ form.addEventListener("submit", (e) => {
   };
 
   console.log(userGame);
+  newGame(userGame);
 });
 
 const newGame = ({ total, type }) => {
-  const howMany = [];
-  const correct = [];
-  const incorrect = [];
-  const review = [];
-
-  let operator;
-
   switch (type) {
     case "addition":
-      operator = param1 + param2;
+      addition(total);
       break;
 
     case "subtraction":
-      operator = param1 - param2;
+      subtraction(total);
       break;
 
     case "multiplication":
-      operator = param1 * param2;
+      multiplication(total);
       break;
 
-    case "subtraction":
-      operator = param1 / param2;
+    case "division":
+      division(total);
       break;
 
     default:
       break;
   }
+};
+
+const multiplication = (total, hard) => {
+  const totalArr = [];
+  const correct = [];
+  const incorrect = [];
+  const review = [];
 
   for (let i = 0; i < total.length; i++) {
     let num1 = Math.floor(Math.random() * 12) + 1;
     let num2 = Math.floor(Math.random() * 12) + 1;
+    let num3 = Math.floor(Math.random() * 12) + 1;
 
-    let question = `${num1} x ${num2} = ?`;
+    let userQuestion = `${num1} x ${num2} = ?`;
+    mathProblem.textContent = "";
+    mathProblem.textContent = userQuestion;
 
-    mathProblem.textContent = question;
-
-    let answer = num1 * num2;
-    let check = userAnswer.value == answer;
+    let anZwer = num1 * num2;
+    let check = userAnswer.value == anZwer;
 
     let newObj = {
-      exercise: question,
+      exercise: userQuestion,
       key: num1 * num2,
-      answer: answer,
+      answer: anZwer,
       correct: check,
     };
 
-    total.push(newObj);
+    totalArr.push(newObj);
   }
-
   total.forEach((index) => {
     if (index.correct == true) {
       correct.push(index);
@@ -88,6 +91,30 @@ const newGame = ({ total, type }) => {
       incorrect.push(index);
     }
   });
+  //Calculate percentage correct
+
+  console.log(
+    `You got ${correct.length} out of ${total.length} correct\nYou scored ${
+      (correct.length / total.length) * 100
+    }%`
+  );
+
+  incorrect.forEach((thing) => {
+    review.push(thing.exercise);
+  });
+
+  if (correct.length != total.length) {
+    console.log(review);
+  }
+
+  let percent = (correct.length / total.length) * 100;
+  finalOutput.textContent = `You got ${correct.length} out of ${
+    total.length
+  } correct\nYou scored ${Math.round(percent)}%\n${
+    percent != 100
+      ? "Here are the problems you missed\n" + review
+      : "Wow! You got them all!"
+  }`;
 };
 
 const multiply = (howMany) => {
