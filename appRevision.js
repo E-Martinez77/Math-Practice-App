4177e5fb6f35390c494c0d8eaafeb5ffafc18426
E-Martinez77 <emartizzle2185@me.com>
2021-12-04 20:18:24 -0700

    it was working then it wasn't

--- appRevision.js
+++ appRevision.js
@@ -1,3 +1,18 @@
+//Create object array via math specific function
+//Push array into global placeholder array
+//At end of void function initiate new function to start the game
+//Populate page with first index
+//Event listener to drive action call function to repeat action
+//Drive app with a global index counter
+//Game active true | false
+//Once array is completed switch game active to false
+//Call function to end game and display score
+//End button to clear everything and start game over
+//Log score total rounds etc to database
+//New page to display high scores
+
+//Possibly explore auth to log in and track personal progress assuming I can get everything else working
+
 const start = document.getElementById("start");
 const startBtn = document.getElementById("start-box");
 const problem = document.getElementById("problem");
@@ -15,26 +30,43 @@ const type = document.getElementById("type");
 const finalOutput = document.getElementById("final-output");
 
 const form = document.getElementById("form");
-const submitBtn = document.getElementById("submitBtn");
+const answerForm = document.getElementById("answer-form");
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  if (!form.checkValidity()) {
-    e.preventDefault();
-    e.stopPropagation();
+const finalDisplay = document.getElementById("final-score");
+const finalScore = document.getElementById("score");
+
+const totalArr = [];
+const correct = [];
+const incorrect = [];
+const review = [];
+let index = 0;
+let gameActive = false;
+
+const displayProblem = () => {
+  if (gameActive == true && index == totalArr.length) {
+    endGame();
   }
+  mathProblem.textContent = "";
+  mathProblem.textContent = totalArr[index].q;
+  userAnswer.value = "";
+};
 
-  form.classList.add("was-validated");
-
-  let howMany = total.value;
-
-  multiplication(howMany);
-  form.classList.add("visually-hidden");
-  problem.classList.remove("visually-hidden");
-});
+const endGame = () => {
+  gameActive = false;
+  problem.classList.add("visually-hidden");
+  finalDisplay.classList.remove("visually-hidden");
+  finalScore.textContent = `You got ${correct.length} out of ${
+    totalArr.length
+  } correct,\nYou scored ${Math.round(
+    (correct.length / totalArr.length) * 100
+  )}%\n${
+    correct.length == totalArr.length
+      ? "Wow! You got them all!"
+      : "Here are the problems you missed\n" + review
+  }`;
+};
 
 const multiplication = (howMany) => {
-  const allProb = [];
   for (let i = 0; i < howMany; i++) {
     let num1 = Math.floor(Math.random() * 12) + 1;
     let num2 = Math.floor(Math.random() * 12) + 1;
@@ -53,12 +85,70 @@ const multiplication = (howMany) => {
         q: `${num1} x ${num2} = ?`,
         a: num1 * num2,
       };
-      allProb.push(newProb);
+      totalArr.push(newProb);
     }
   }
 
-  console.log(allProb);
-
-  let index = 0;
-  mathProblem.textContent = allProb[index].q;
+  console.log(totalArr);
+  displayProblem();
 };
+
+const newGame = (choice, howMany) => {
+  switch (choice) {
+    case "addition":
+      addition(howMany);
+      break;
+    case "subtraction":
+      subtraction(howMany);
+      break;
+    case "multiplication":
+      multiplication(howMany);
+      break;
+    case "division":
+      division(howMany);
+      break;
+
+    default:
+      break;
+  }
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (!form.checkValidity()) {
+    e.preventDefault();
+    e.stopPropagation();
+  }
+
+  form.classList.add("was-validated");
+
+  let howMany = total.value;
+  let choice = type.value;
+
+  form.classList.add("visually-hidden");
+  problem.classList.remove("visually-hidden");
+
+  newGame(choice, howMany);
+});
+
+answerForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  if (userAnswer.textContent == "") {
+    e.preventDefault();
+  }
+
+  if (userAnswer.textContent == totalArr[index].a) {
+    correct.push(totalArr[index]);
+    index++;
+    displayProblem();
+  } else {
+    review.push(totalArr[index].q);
+    index++;
+    displayProblem();
+  }
+});
+
+if (index == totalArr.length) {
+  endGame();
+}
